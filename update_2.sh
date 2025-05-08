50842958f614fc2423d31d9a1b94a78b79e4c937
ᥴ̶̧̪̤̓͊̌̽̊̎̓͜͝͠ꪮ̵͈̀͐͗͒̽͛͊͝ꪀ̸̧̨̺̮̩̣̤͂̾͜ᦓ̵̠̂̅̈́̊ͅꪮ̶̢͇̟͆͗͆́̌͗͂͒̀͜͝ꪶ̴̨̧̖͍̞̲̖̠̆̽̈́̇̾̍̒̕͠ͅꫀ̵̡̨̧̛͚͕̝̭̦̩̻̈́̚̚ͅ <162306504+cons0le7@users.noreply.github.com>
2025-05-08 12:17:15 -0700

    Update update_2.sh

--- update_2.sh
+++ update_2.sh
@@ -3,9 +3,9 @@
 chmod +x "$HOME/nosint/nosint"
 chmod +x "$HOME/nosint/update.sh"
 echo ""
-echo "Update Successful. Press Enter to reload script."
+echo "Update Successful."
 echo ""
-echo "reloading... "
+echo "reloading tool... "
 sleep 1 
-"$HOME/nosint/nosint"
+exec "nosint"
 
