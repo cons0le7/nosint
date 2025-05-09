d9a4a9c7db2e016b46336aa62347fa8e162f376f
ᥴ̶̧̪̤̓͊̌̽̊̎̓͜͝͠ꪮ̵͈̀͐͗͒̽͛͊͝ꪀ̸̧̨̺̮̩̣̤͂̾͜ᦓ̵̠̂̅̈́̊ͅꪮ̶̢͇̟͆͗͆́̌͗͂͒̀͜͝ꪶ̴̨̧̖͍̞̲̖̠̆̽̈́̇̾̍̒̕͠ͅꫀ̵̡̨̧̛͚͕̝̭̦̩̻̈́̚̚ͅ <162306504+cons0le7@users.noreply.github.com>
2025-05-09 15:02:57 +0000

    Add files via upload

---
+++ update/update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+HOME_PATH="$HOME/nosint"
+REPO_URL="https://github.com/cons0le7/nosint.git"
+
+if [ ! -d "$HOME_PATH" ]; then
+    echo ""
+    git clone "$REPO_URL" "$HOME_PATH" 
+    chmod +x "$HOME_PATH/update.sh"
+    chmod +x "$HOME_PATH/update_2.sh"
+    exec "$HOME_PATH/update_2.sh"
+    exit 0
+else
+    echo ""
+    git -C "$HOME_PATH" fetch
+    BRANCH_NAME=$(git -C "$HOME_PATH" rev-parse --abbrev-ref HEAD)
+    git -C "$HOME_PATH" reset --hard "origin/$BRANCH_NAME"
+    chmod +x "$HOME_PATH/update.sh"
+    chmod +x "$HOME_PATH/update_2.sh"
+    exec "$HOME_PATH/update_2.sh"
+    exit 0 
+fi
---
+++ update/update_2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+chmod +x "$HOME/nosint/nosint"
+chmod +x "$HOME/nosint/update.sh"
+echo ""
+echo "Update Successful."
+echo ""
+echo "Type 'nosint' to reload script: "
+exit 0
+
