7499784895cea3926bff740ddc334899e30992fc
ᥴ̶̧̪̤̓͊̌̽̊̎̓͜͝͠ꪮ̵͈̀͐͗͒̽͛͊͝ꪀ̸̧̨̺̮̩̣̤͂̾͜ᦓ̵̠̂̅̈́̊ͅꪮ̶̢͇̟͆͗͆́̌͗͂͒̀͜͝ꪶ̴̨̧̖͍̞̲̖̠̆̽̈́̇̾̍̒̕͠ͅꫀ̵̡̨̧̛͚͕̝̭̦̩̻̈́̚̚ͅ <162306504+cons0le7@users.noreply.github.com>
2025-05-09 08:04:54 -0700

    Update update.sh

--- update/update.sh
+++ update/update.sh
@@ -6,17 +6,17 @@ REPO_URL="https://github.com/cons0le7/nosint.git"
 if [ ! -d "$HOME_PATH" ]; then
     echo ""
     git clone "$REPO_URL" "$HOME_PATH" 
-    chmod +x "$HOME_PATH/update.sh"
-    chmod +x "$HOME_PATH/update_2.sh"
-    exec "$HOME_PATH/update_2.sh"
+    chmod +x "$HOME_PATH/update/update.sh"
+    chmod +x "$HOME_PATH/update/update_2.sh"
+    exec "$HOME_PATH/update/update_2.sh"
     exit 0
 else
     echo ""
     git -C "$HOME_PATH" fetch
     BRANCH_NAME=$(git -C "$HOME_PATH" rev-parse --abbrev-ref HEAD)
     git -C "$HOME_PATH" reset --hard "origin/$BRANCH_NAME"
-    chmod +x "$HOME_PATH/update.sh"
-    chmod +x "$HOME_PATH/update_2.sh"
-    exec "$HOME_PATH/update_2.sh"
+    chmod +x "$HOME_PATH/update/update.sh"
+    chmod +x "$HOME_PATH/update/update_2.sh"
+    exec "$HOME_PATH/update/update_2.sh"
     exit 0 
 fi
